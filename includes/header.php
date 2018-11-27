78f2d0c518411c49a8e560232667e433d6698059
ppe20162017 <tanguy.equinet@gastonberger.fr>
2018-11-27 21:37:19 +0100

    Menu + slider

--- includes/header.php
+++ includes/header.php
@@ -10,19 +10,19 @@
             <div class="col-lg-8 menu" style="margin-top: 9%;">
                 <ul class="nav ">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Accueil</a>
+                        <a class="nav-link active" href="/">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Compétences</a>
+                        <a class="nav-link active" href="/competences.php">Compétences</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">L'Etude</a>
+                        <a class="nav-link" href="/etude.php">L'Etude</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">L'équipe</a>
+                        <a class="nav-link" href="/equipe.php">L'équipe</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Coordonnées</a>
+                        <a class="nav-link" href="/coordonnees.php">Coordonnées</a>
                     </li>
                 </ul>
             </div> 
@@ -38,24 +38,23 @@
                    
                     <li class="nav-item" style="width: 35%;">
                     <a href="http://127.0.0.1/Notaire/" title="Etude de Maître Florence RICHARD, Notaire à WASQUEHAL">
-                    <img src="http://127.0.0.1/Notaire/includes/assets/logo.png" alt="logo" >
+                    <img   src="http://127.0.0.1/Notaire/includes/assets/logo.png" alt="logo" >
                 </a>
                     </li>
                     <li class="nav-item" style="margin-top: 5%;">
-                    <select class="form-control form-control-lg">
-                        <option>Accueil</option>
-                        <option>Competences</option>
-                        <option>L'Etudes</option>
-                        <option>L'équipe</option>
-                        <option>Coordonnées</option>
+                    <select class="form-control form-control-lg" onchange="document.location.href=this.value">
+                        <option value="/">Accueil</option>
+                        <option value="/competences.php">Competences</option>
+                        <option value="/etude.php">L'Etudes</option>
+                        <option value="/equipe.php">L'équipe</option>
+                        <option value="/coordonnees.php">Coordonnées</option>
                     </select>
+                   
                     </li>
                 </ul>          
                 
             </div>
-            <div class="col-lg-1 menu" style="margin-top: 9%;">
             
-            </div> 
         </div>
     </div>                  
 </div>

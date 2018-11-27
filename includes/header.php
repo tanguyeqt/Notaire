dcedc64bc538fb88a662e02754201b00a35dd3c6
ppe20162017 <tanguy.equinet@gastonberger.fr>
2018-11-27 20:32:09 +0100

    Menu ok

--- includes/header.php
+++ includes/header.php
@@ -1,33 +1,61 @@
 <header id="banniere">
-
-<div class="container">
-    <div class="row">       
-        <div class="nav-menu">
-            <nav class="navbar navbar-expand-lg navbar-light logo menu">
+<div class="nav-menu logo desktop">
+    <div class="container">
+        <div class="row">    
+            <div class="col-lg-4" style="margin-top: 2%;">
                 <a href="http://127.0.0.1/Notaire/" title="Etude de Maître Florence RICHARD, Notaire à WASQUEHAL">
-                    <img src="http://127.0.0.1/Notaire/includes/assets/logo.png"  alt="logo" >
+                    <img src="http://127.0.0.1/Notaire/includes/assets/logo.png" class="img-responsive center-block"  alt="logo" >
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            </div>
+            <div class="col-lg-8 menu" style="margin-top: 9%;">
+                <ul class="nav ">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Compétences</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">L'Etude</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">L'équipe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Coordonnées</a>
+                    </li>
+                </ul>
+            </div> 
+        </div>
+    </div>                  
+</div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="competences.php">Competences</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="etudes.php">L'Etude</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="equipe.php">L'équipe</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="coordonnees.php">Coordonnées</a>
-                        </li>   
-                     </ul>    
-                </div>
-            </nav> 
+<div class="nav-menu logo mobile">
+    <div class="container">
+        <div class="row">    
+            <div class="col-lg-1" style="margin-top: 5%;">
+            <ul class="nav ">
+                   
+                    <li class="nav-item" style="width: 35%;">
+                    <a href="http://127.0.0.1/Notaire/" title="Etude de Maître Florence RICHARD, Notaire à WASQUEHAL">
+                    <img src="http://127.0.0.1/Notaire/includes/assets/logo.png" alt="logo" >
+                </a>
+                    </li>
+                    <li class="nav-item" style="margin-top: 5%;">
+                    <select class="form-control form-control-lg">
+                        <option>Accueil</option>
+                        <option>Competences</option>
+                        <option>L'Etudes</option>
+                        <option>L'équipe</option>
+                        <option>Coordonnées</option>
+                    </select>
+                    </li>
+                </ul>          
+                
+            </div>
+            <div class="col-lg-1 menu" style="margin-top: 9%;">
+            
+            </div> 
         </div>
     </div>                  
 </div>
